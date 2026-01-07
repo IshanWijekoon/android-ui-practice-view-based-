50485ae925eec81fe871e70aca1d76b81444861d
Ishan Madusanka <ishancoc295@gmail.com>
2026-01-07 09:40:17 +0530

    build first interaction with the user. (welcome message)

--- app/src/main/java/com/example/views_basedproject/MainActivity.kt
+++ app/src/main/java/com/example/views_basedproject/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.views_basedproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.Hello)
+        val inputField = findViewById<EditText>(R.id.edit_text)
+        val btn = findViewById<Button>(R.id.submit_btn)
+        btn.setOnClickListener {
+            val enteredName = inputField.text.toString()
+            val message = "Wellcome $enteredName"
+            greetingTextView.text = message
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
