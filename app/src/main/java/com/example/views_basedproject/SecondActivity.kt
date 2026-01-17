3c30f66e6fbf775b85de61ed493083f4c2ef8e07
Ishan Madusanka <ishancoc295@gmail.com>
2026-01-17 13:25:26 +0530

    Navigate to the second activity and displays a message

--- app/src/main/java/com/example/views_basedproject/SecondActivity.kt
+++ app/src/main/java/com/example/views_basedproject/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.views_basedproject
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,12 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
+
+        val userName = intent.getStringExtra("USER")
+        val textView = findViewById<TextView>(R.id.tvOffer)
+        val message = "$userName ,you will get free access to all the content for one month"
+        textView.text = message
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
